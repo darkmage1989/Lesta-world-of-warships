a173cf95fd0aedb0f879b7a9355698ca3763a177
darkmage1989 <darkmage1989@gmail.com>
2023-10-22 22:39:37 +0300

    Инициализация проекта

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import {ApolloProvider} from '@apollo/client'
+import {client} from './apollo/apollo'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ApolloProvider client={client}>
       <App />
-    </Provider>
+      </ApolloProvider>
   </React.StrictMode>
 );
 
